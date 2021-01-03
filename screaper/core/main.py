9a0c134794289ba9b6646736c6c39f00081e75b0
David <david.yenicelik@gmail.com>
2021-01-03 11:19:49 +0100

    Splitting up function logic s.t. function is run similar to threads

--- screaper/core/main.py
+++ screaper/core/main.py
@@ -2,12 +2,10 @@
     Includes the main application loop
 """
 import asyncio
-import time
 
 import requests
 
 from screaper.crawl_frontier.crawl_frontier import CrawlFrontier
-from screaper.downloader.downloader import Downloader
 
 from screaper.downloader.async_crawl_task import CrawlAsyncTask
 from screaper_resources.resources.db import Database
@@ -24,29 +22,37 @@ class Main:
         self.resource_database = Database()  # TODO Make database async!
 
         self.crawl_frontier = CrawlFrontier(resource_database=self.resource_database)
-        self.downloader = Downloader(resource_database=self.resource_database, proxy_list=self.proxy_list)
 
         # Implement an async-queue
 
         self.ping_interval = 5  # How many seconds to run the ping interval for
 
-    def run_main_loop(self, max_sites=None):
+        self.crawl_task_queue = asyncio.Queue(maxsize=513)
+        self.active_workers_count = 0
 
-        crawl_task_queue = asyncio.Queue(maxsize=513)
-        executed_crawl_task_queue = asyncio.Queue(maxsize=513)
-        active_workers_count = 0
+    def execute_single_task_from_queue(self, max_sites=None):
+
+        # Fetch from queue
+
+        ##############
+        # If queue is shallow or empty, populate queue
+        ##############
+
+        # Else, run worker
+        pass
+
+    async def run_main_loop(self):
 
         c = 0
 
         while True:
 
-            ##############
-            # Populate queue if queue is not big enough
-            ##############
+            # if isinstance(max_sites, int) and c > max_sites:
+            #     print("Number of maximum crawled sites through this thread done", c, max_sites)
+            #     return
 
-            if isinstance(max_sites, int) and c > max_sites:
-                print("Number of maximum crawled sites through this thread done", c, max_sites)
-                return
+            # Run multiple of the self.execute_single_task_from_queue instances.
+            # This will act as if many threads are simultenously running
 
             # Fetch more items into the queue
             if not crawl_task_queue.full():
@@ -55,27 +61,48 @@ class Main:
                 # Do multiple retries of this, and fail gracefully.
                 # Nevermind, there will be multiple retries anyways
                 queue_objs_to_crawl = self.crawl_frontier.pop_start_list()
+                tasks_to_await = []
                 for queue_obj in queue_objs_to_crawl:
                     # TODO: Gotta spawn a coroutine from this here
                     markup_exists = self.resource_database.get_markup_exists(queue_obj.url)  # TODO make async
                     if not markup_exists and queue_obj.url:
                         crawl_task = CrawlAsyncTask(self.proxy_list, queue_obj)
-                        crawl_task_queue.put(crawl_task)
+                        # TODO why await
+                        crawl_task_queue_put_task = crawl_task_queue.put(crawl_task)
+                        tasks_to_await.append(crawl_task_queue_put_task)
                     else:
                         print("Markup already exists", queue_obj.url)
                         # Mark this as processed
                         self.resource_database.get_url_task_queue_record_completed(url=queue_obj.url)
                 self.resource_database.commit()
 
+                # Similar to blocking really, but allows for parallellism
+                for crawl_task_queue_put_task in tasks_to_await:
+                    await crawl_task_queue_put_task
+
+                print("Queue is: ", len(queue_objs_to_crawl), queue_objs_to_crawl)
+
+            tasks_to_await = []
             # Spawn a bunch of async tasks
             for i in range(active_workers_count):
-                crawl_task = crawl_task_queue.get()
+                get_crawl_task = crawl_task_queue.get()
+                tasks_to_await.append(get_crawl_task)
+
+            # TODO: Put this in another section where you have to await the entire pipeline?
+            for get_crawl_task in tasks_to_await:
+                await get_crawl_task
+                print("Get crawl task is: ", get_crawl_task)
+
+
+                # TODO: Does this make sure it gets run like a thread, i.e. in parallel?
                 executed_crawl_task = asyncio.run(crawl_task)
 
                 # Put them into the next queue, if they don't return None
 
                 # Complete bullshit pseudocode
-                executed_crawl_task_queue.put(executed_crawl_task)
+                await executed_crawl_task_queue.put(executed_crawl_task)
+
+            exit(0)
 
             if not crawl_task_queue.empty():
                 # Go through all executed tasks, and process them into the database one after the other
@@ -124,5 +151,4 @@ if __name__ == "__main__":
     print("Starting the application")
     main = Main()
 
-
-    main.run_main_loop()
+    asyncio.run(main.run_main_loop())
