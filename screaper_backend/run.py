f7e2df704fe278dfb939e347c43953a0a7750b4b
David <david.yenicelik@gmail.com>
2021-01-16 14:40:42 +0100

    Added port descriptor

--- screaper_backend/run.py
+++ screaper_backend/run.py
@@ -7,4 +7,4 @@ from screaper_backend.application.application import application
 if __name__ == "__main__":
     print("Starting flask application ")
     # port=environ.get("PORT"),
-    application.run(host="0.0.0.0", debug=True)
+    application.run(host="0.0.0.0", port=environ.get("PORT"), debug=True)
