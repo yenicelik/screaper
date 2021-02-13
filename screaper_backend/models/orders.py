10dfe37d6d4c122fbba6ad9f9d2b7b0e94c9244d
David <david.yenicelik@gmail.com>
2021-02-13 19:07:57 +0100

    openpyxl added

--- screaper_backend/models/orders.py
+++ screaper_backend/models/orders.py
@@ -11,7 +11,7 @@ load_dotenv()
 class Orders:
 
     def __init__(self):
-        print("Current pwd is: ", os.getcwd())
+        print("Current pwd is: ", os.getcwd(), os.listdir(os.getcwd()))
         with open(os.getenv("MOCK_ORDERS"), "r") as f:
             loaded_yaml_file = yaml.load(f, Loader=yaml.Loader)['orders']
 
