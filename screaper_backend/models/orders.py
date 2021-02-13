323cb7dc3829faa3e2858cb74702440b6236e854
David <david.yenicelik@gmail.com>
2021-02-13 18:26:43 +0100

    openpyxl added

--- screaper_backend/models/orders.py
+++ screaper_backend/models/orders.py
@@ -2,12 +2,16 @@
     Get orders for a certain user
 """
 import yaml
+import os
 
+from dotenv import load_dotenv
+
+load_dotenv()
 
 class Orders:
 
     def __init__(self):
-        with open("/Users/david/screaper/screaper_backend/mockups_order.yaml", "r") as f:
+        with open(os.getenv("MOCK_ORDERS"), "r") as f:
             loaded_yaml_file = yaml.load(f, Loader=yaml.Loader)['orders']
 
         self._orders = loaded_yaml_file
