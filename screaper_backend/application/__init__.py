188c5eaa793b9eff7e6b6dcef0e10d3460feae41
David <david.yenicelik@gmail.com>
2021-03-02 21:09:59 +0100

    updated database information

--- screaper_backend/application/__init__.py
+++ screaper_backend/application/__init__.py
@@ -9,7 +9,7 @@ load_dotenv()
 
 # These need to run before further imports, otherwise circular (maybe just put them into __init__
 application = Flask(__name__)
-application.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DatabaseUrlApplication')
+application.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE')
 application.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 CORS(application)
 db = SQLAlchemy(application)
