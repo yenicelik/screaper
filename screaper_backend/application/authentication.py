7c7b2046e941488ad47452caf5f4977499be8f91
David <yenicelik@users.noreply.github.com>
2021-03-31 15:37:16 +0200

    Update authentication.py

--- screaper_backend/application/authentication.py
+++ screaper_backend/application/authentication.py
@@ -31,5 +31,6 @@ whitelist_emails = [
     "mcyenicelik@hotmail.com",
     "baker@bakermagnetics.com.tr",
     "auguryenicelik@hotmail.com",
+    "mmarinayenicelik@hotmail.com",
     "mcyenicelik@bakermagnetics.com.tr"
 ]
