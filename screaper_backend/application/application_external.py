9fdc57a7aa98f264a33e4d8f711fea5c580d5999
David <david.yenicelik@gmail.com>
2021-05-11 12:49:51 +0200

    will continue with normal application

--- screaper_backend/application/application_external.py
+++ screaper_backend/application/application_external.py
@@ -681,3 +681,73 @@ def external_orders_confirm():
     return jsonify({
         "response": "Order successfully filled!"
     }), 200
+
+@application.route('/external/companies', methods=["GET", "POST"])
+@check_authentication_token
+def external_list_companies():
+    """
+        Example request could look as follows:
+        {
+            "query": "Union"
+        }
+
+    """
+    try:
+        input_json = json.loads(request.data, strict=False)
+    except Exception as e:
+        print("Request body could not be parsed!", str(e), str(request.data))
+        return jsonify({
+            "errors": ["Request body could not be parsed!", str(e), str(request.data)]
+        }), 400
+
+    print("Got request: ", input_json)
+
+    if "query" not in input_json:
+        return jsonify({
+            "errors": ["query not fund!", str(input_json)]
+        }), 400
+
+    if not input_json["query"]:
+        return jsonify({
+            "errors": ["query empty!", str(input_json)]
+        }), 400
+
+    # # Check if the api token is valid!
+    # header_token = request.headers.get('token')
+    # if isinstance(header_token, str):
+    #     header_token = header_token.upper()
+
+    # Make type validation
+    # Calculate the location from this
+
+    # Retrieve input string
+    query = input_json["query"]
+
+    # Pass the query through the model
+    tmp_out = algorithm_product_similarity.predict(query)
+
+    out = []
+    for x in tmp_out:
+        keys_to_delete = set()
+        for key in x.keys():
+            # whitelabel allowed properties
+            if key not in [
+                "description_de",
+                "description_en",
+                "id",
+                "manufacturer",
+                "manufacturer_abbreviation",
+                "part_external_identifier",
+                "sequence_order",
+            ]:
+                keys_to_delete.add(key)
+        for key in keys_to_delete:
+            del x[key]
+        out.append(x)
+
+    # Save into the json
+    print("out items are: ", out)
+
+    return jsonify({
+        "response": out
+    }), 200
