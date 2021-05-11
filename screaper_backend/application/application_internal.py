6510ec0a0bd877e27623fbd2d43a23e22d5f21c0
David <david.yenicelik@gmail.com>
2021-05-11 19:24:19 +0200

    will now make next iteration of MVP

--- screaper_backend/application/application_internal.py
+++ screaper_backend/application/application_internal.py
@@ -147,6 +147,50 @@ def internal_orders_get():
     }), 200
 
 
+@application.route('/internal/orders-get-single', methods=["GET", "POST"])
+@check_authentication_token
+def internal_orders_get_single():
+    """
+        Example request could look as follows:
+        {}
+
+    """
+    try:
+        input_json = json.loads(request.data, strict=False)
+    except Exception as e:
+        print("Request body could not be parsed!", str(e), str(request.data))
+        return jsonify({
+            "errors": ["Request body could not be parsed!", str(e), str(request.data)]
+        }), 400
+
+    # Check again if this is an admin e-mail
+    if request.user['email'] not in admin_emails:
+        return jsonify({
+            "errors": ["Permission denied (F.002)", str(request.headers)]
+        }), 403
+
+    print("Got request: ", input_json)
+
+    # Get orders for this user only!
+    print("User is: ", request.user)
+    user_email = request.user.get("email")
+    order_id = input_json["order_id"]
+
+    print("User uuid and user email are: ", user_email)
+    print("Order id is: ", order_id)
+
+    order = model_orders.order_by_order_id(order_id=order_id)
+    if order is None:
+        print("errors", ["No corresponding orders are found!", str(request.data)])
+        return jsonify({
+            "errors": ["No corresponding orders are found!", str(request.data)]
+        }), 400
+
+    return jsonify({
+        "response": order
+    }), 200
+
+
 @application.route('/internal/orders-edit', methods=["GET", "POST"])
 @check_authentication_token
 def internal_orders_edit():
